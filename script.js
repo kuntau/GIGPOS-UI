ac15b4ef005199bdbda2368999d71443562d521a
Nizamuddin Sulieman <kuntau17@gmail.com>
2020-04-09 22:46:33 +0800

    sidebar value from vue

--- script.js
+++ script.js
@@ -1,10 +1,34 @@
 const sideNav = [
-  "Dashboard",
-  "Reports",
-  "Charts",
-  "Produts",
-  "Inbox",
-  "Settings"
+  {
+    name: 'Dashboard',
+    url: 'dashboard.html',
+    active: true
+  },
+  {
+    name: 'Reports',
+    url: 'reports.html',
+    active: false
+  },
+  {
+    name: 'Charts',
+    url: 'charts.html',
+    active: false
+  },
+  {
+    name: 'Products',
+    url: 'products.html',
+    active: false
+  },
+  {
+    name: 'Inbox',
+    url: 'inbox.html',
+    active: false
+  },
+  {
+    name: 'Settings',
+    url: 'settings.html',
+    active: false
+  }
 ];
 
 const topNavLeft = ["Home", "Name"];
