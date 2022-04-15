2dddd07660a895d64fcb6b3beebb89bd6d9bc800
Nizamuddin Sulieman <kuntau17@gmail.com>
2022-04-16 04:13:07 +0800

    chore(dev): add alias, eslint ts, ignore watch

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,23 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import WindiCSS from 'vite-plugin-windicss'
+import * as path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      'vue': 'vue/dist/vue.esm-bundler.js'
+    }
+  },
+  server: {
+    watch: {
+      ignored: [
+        '**/public/fonts/**'
+      ]
+    }
+  },
   plugins: [
     vue(),
     WindiCSS()
