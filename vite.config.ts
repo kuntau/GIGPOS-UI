8214409453f574fce6a729dbd85f555b8a34d3e5
Nizamuddin Sulieman <kuntau17@gmail.com>
2022-11-16 10:41:43 +0800

    chore(pnpm): update dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,10 @@ export default defineConfig({
     }
   },
   server: {
+    host: true,
+    port: 3000,
+    strictPort: true,
+    open: false,
     watch: {
       ignored: [
         '**/public/fonts/**'
