6dd4a3e3e2d9a17ce7584a31abcff630e646135e
Nizamuddin Sulieman <kuntau17@gmail.com>
2022-11-29 23:34:25 +0800

    feat(Dashboard): Try use chart.js

--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -1,9 +1,16 @@
 <script setup lang='ts'>
-import { useCounter } from '../stores/gigpos';
-const counter = useCounter();
+import DailySales from '../components/Dashboard/DailySales.vue'
+import WeeklySales from '../components/Dashboard/WeeklySales.vue'
+import BarChart from '../components/Dashboard/BarChart'
 
-const reset = () => {
-  counter.$reset()
+const chartData = {
+  labels: [ 'January', 'February', 'March', 'AprilX' ],
+  dataset: [ { data: [ 40, 20, 12, 35 ] }
+  ]
+}
+
+const chartOptions = {
+  responsive: true
 }
 
 defineProps<{
@@ -16,10 +23,8 @@ defineProps<{
     <h1>This is dashboard</h1>
     <h2>With props <code>page</code> {{ page }}</h2>
     <br />
-    <h3>With counter store <code>{{ counter }}</code> and count <code>{{ counter.count }}</code>. </h3>
-    <button class="bg-green-200 px-4 py-1 rounded" @click="counter.increment">+</button>
-    <button class="bg-red-200 px-4 py-1 rounded" @click="counter.decrement">-</button>
-    <button class="bg-blue-200 px-4 py-1 rounded" @click="counter.fixed">set</button>
-    <button class="bg-purple-200 px-4 py-1 rounded" @click="reset">reset</button>
+    <DailySales chart-id="daily-saless" :chart-data="chartData" />
+    <WeeklySales :chart-data="chartData" />
+    <BarChart chart-id="daily-chart" :chart-data="chartData" />
   </div>
 </template>
