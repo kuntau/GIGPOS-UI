ba3c9625d3a14a33623679371b59108531816aa5
Nizamuddin Sulieman <kuntau17@gmail.com>
2022-12-01 04:37:27 +0800

    feat(chartjs): make generic Bar and Line chart
    - Clean up
    - LineChart with fill
    - Responsive still not working :(

--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -2,23 +2,6 @@
 import DailySales from '../components/Dashboard/DailySales.vue'
 import WeeklySales from '../components/Dashboard/WeeklySales.vue'
 
-const chartData = {
-  labels: [ 'January', 'February', 'March', 'AprilX' ],
-  datasets: [
-    {
-      data: [ 40, 20, 12, 35 ],
-      borderColor: '#86A2EB',
-      /* backgroundColor: '#9BD0F5', */
-      backgroundColor: '#669010',
-    }
-  ]
-}
-
-const chartOptions = {
-  responsive: false,
-  maintainAspectRatio: false
-}
-
 defineProps<{
   page: string,
 }>()
@@ -27,9 +10,9 @@ defineProps<{
 <template>
   <div class="dashboard">
     <h1>This is dashboard</h1>
-    <h2>With props <code>page</code> {{ page }}</h2>
-    <br />
-    <DailySales chart-id="daily-sales" :chart-data="chartData" :chart-options="chartOptions" />
+    <div class="mt-5">
+      <DailySales />
+    </div>
     <!-- <WeeklySales :chart-data="chartData" /> -->
   </div>
 </template>
