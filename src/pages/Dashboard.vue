12b5c88c7cc8046da5ce47752ff28b11871290da
Nizamuddin Sulieman <kuntau17@gmail.com>
2022-12-01 01:55:22 +0800

    feat(chartjs): play with **Scriptable Options**
    references:
    - [docs](https://www.chartjs.org/docs/latest/general/options.html#scriptable-options)
    - Import type from chart.js
    - Scriptable options type is `ScriptableChartContext`
    - or `ScriptableContext` with chart type generics

--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -4,7 +4,13 @@ import WeeklySales from '../components/Dashboard/WeeklySales.vue'
 
 const chartData = {
   labels: [ 'January', 'February', 'March', 'AprilX' ],
-  datasets: [ { data: [ 40, 20, 12, 35 ] }
+  datasets: [
+    {
+      data: [ 40, 20, 12, 35 ],
+      borderColor: '#86A2EB',
+      /* backgroundColor: '#9BD0F5', */
+      backgroundColor: '#669010',
+    }
   ]
 }
 
@@ -23,7 +29,7 @@ defineProps<{
     <h1>This is dashboard</h1>
     <h2>With props <code>page</code> {{ page }}</h2>
     <br />
-    <DailySales chart-id="daily-saless" :chart-data="chartData" :chart-options="chartOptions" />
+    <DailySales chart-id="daily-sales" :chart-data="chartData" :chart-options="chartOptions" />
     <!-- <WeeklySales :chart-data="chartData" /> -->
   </div>
 </template>
